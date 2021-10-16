e6caa1e968587cf9088bb346181e5f3644249843
Jill Menning <jill@jmenning.com>
2021-10-16 17:14:00 -0500

    Separated routes to make them easier to update

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
+<?php 
 
+function loadPage($page) {
+    include "header.php";
+    include($page);
+    include "footer.php";
+}
 
-<?php include "routes.php" ?>
+function loadArticle($fileName) {
+    include "header.php";
+
+    $html = processMarkdown($fileName);
+    echo "<article>".$html."</article>";
+
+    include "footer.php";
+}
+
+$request = $_SERVER['REQUEST_URI'];
+$request = rtrim($request, '/\\'); // ignore trailing slashes
+
+// routes are in their own file
+include "routes.php";
+
+if (isset($pageRoutes[$request])) {
+    loadPage($pageRoutes[$request]);
+} else if (isset($markdownFiles[$request])) {
+    loadArticle($markdownFiles[$request]);
+} else {
+    http_response_code(404);
+    include('404.php');
+}
+
+?>
 
--- routes.php
+++ routes.php
@@ -1,23 +1,5 @@
 <?php
 
-    function loadPage($page) {
-        include "header.php";
-        include($page);
-        include "footer.php";
-    }
-
-    function loadArticle($fileName) {
-        include "header.php";
-
-        $html = processMarkdown($fileName);
-        echo "<article>".$html."</article>";
-
-        include "footer.php";
-    }
-
-    $request = $_SERVER['REQUEST_URI'];
-    $request = rtrim($request, '/\\'); // ignore trailing slashes
-
     $pageRoutes = array(
         "" => "home.php",
         "/" => "home.php",
@@ -31,14 +13,5 @@
         "/highlight-menu-location" => "personal/content/highlight-menu-location.md",
         "/links-in-php-and-html" => "personal/content/links-in-php-and-html.md"
     );
-
-    if (isset($pageRoutes[$request])) {
-        loadPage($pageRoutes[$request]);
-    } else if (isset($markdownFiles[$request])) {
-        loadArticle($markdownFiles[$request]);
-    } else {
-        http_response_code(404);
-        include('404.php');
-    }
-
+    
 ?>
