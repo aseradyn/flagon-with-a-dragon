9bb78c1f346ea9acfe167198c82331a626d68b1c
Jill Menning <jill@jmenning.com>
2022-02-10 19:02:32 -0600

    experiment

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ include($_SERVER["DOCUMENT_ROOT"]."/theme/header.php");
 
 include($_SERVER["DOCUMENT_ROOT"]."/router/router.php");
 
+// test!
+putenv("tmp=usr");
+
 ?>
 
 <div style="margin-bottom: 40px;"></div>
