700d1291858648ccd3c6668e9ce01db47466fb20
Jill Menning <jill@jmenning.com>
2022-02-10 19:48:45 -0600

    revert experiment

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ include($_SERVER["DOCUMENT_ROOT"]."/theme/header.php");
 
 include($_SERVER["DOCUMENT_ROOT"]."/router/router.php");
 
-// test!
-putenv("tmp=usr");
-
 ?>
 
 <div style="margin-bottom: 40px;"></div>
