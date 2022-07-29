95c7f10578583d90fa2beccba77707fff4d4d4c8
Jill Menning <jill@jmenning.com>
2022-07-28 19:35:29 -0500

    Get variables correctly

--- auth/auth0_initialization.php
+++ auth/auth0_initialization.php
@@ -8,10 +8,10 @@ use Auth0\SDK\Configuration\SdkConfiguration;
 
 $configuration = new SdkConfiguration(
     domain: 'dev-fisjgu7p.us.auth0.com',
-    clientId: $_ENV["CLIENT_ID"],
-    clientSecret: $_ENV["CLIENT_SEC"],
+    clientId: getenv("CLIENT_ID"),
+    clientSecret: getenv("CLIENT_SECRET"),
     redirectUri: 'http://' . $_SERVER['HTTP_HOST'] . "/callback",
-    cookieSecret: $_ENV["COOKIE_SEC"]
+    cookieSecret: getenv("COOKIE_SECRET")
 );
 
 $auth0 = new Auth0($configuration);
