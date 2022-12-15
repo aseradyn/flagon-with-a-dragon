0af9c24fe92603bafa4e6cc4fc03aa73e4d36e66
Jill Menning <jill@jmenning.com>
2022-12-15 07:46:45 -0600

    Change location for variables

--- auth/auth0_initialization.php
+++ auth/auth0_initialization.php
@@ -2,16 +2,17 @@
 declare(strict_types=1);
 
 require('vendor/autoload.php');
+require('../configurations/authzero.php');
 
 use Auth0\SDK\Auth0;
 use Auth0\SDK\Configuration\SdkConfiguration;
 
 $configuration = new SdkConfiguration(
-    domain: 'dev-fisjgu7p.us.auth0.com',
-    clientId: getenv("CLIENT_ID"),
-    clientSecret: getenv("CLIENT_SECRET"),
+    domain: $AUTH0_DOMAIN,
+    clientId: $AUTH0_CLIENT_ID,
+    clientSecret: $AUTH0_CLIENT_SECRET,
     redirectUri: 'https://' . $_SERVER['HTTP_HOST'] . "/auth/callback",
-    cookieSecret: getenv("COOKIE_SECRET")
+    cookieSecret: $AUTH0_COOKIE_SECRET
 );
 
 $auth0 = new Auth0($configuration);
