6e813788fc65a98234ddfae393b22085c3808500
Jill Menning <jill@jmenning.com>
2023-01-01 16:01:35 -0600

    I really need a local WP environment

--- blog/wp-content/themes/jm2023/index.php
+++ blog/wp-content/themes/jm2023/index.php
@@ -9,10 +9,9 @@
     add_filter( 'wp_get_attachment_image_attributes', 'apply_class', 10 );
 ?>
 
-
 <style>
-    img {
-        max-width: 100%;
+    .blog-thumbnail {
+        width: 75px;
     }
 </style>
 
@@ -26,15 +25,16 @@
                 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                 </h2>
             </header>
+            
+            <p><?php the_date() ?></p>
             <?php 
                 if ( has_post_thumbnail() ) {
-                    //the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) );
-                    the_post_thumbnail();
+                    the_post_thumbnail( 'thumbnail', array( 'class' => 'photo-card blog-thumbnail' ) );
+                    //the_post_thumbnail();
                 }
-                the_date();
-                //the_content(); 
+            ?>
+            <?php 
                 the_excerpt();
-                //wp_link_pages(); 
                 edit_post_link(); 
             ?>
         </section>
