64a4f3af59adf7f95233ef3c81e2b9b5d964bd14
Jill Menning <jill@jmenning.com>
2023-05-28 20:22:19 -0500

    Add webring links

--- pages/index.php
+++ pages/index.php
@@ -39,6 +39,19 @@
 
     </section>
 
+    <section>
+    <table>
+	<tr>
+		<td colspan="3">This site is a member of the CS.Sjoy.lol webring</td>
+	</tr>
+	<tr>
+		<td style="text-align: center"><a href="https://webri.ng/webring/cssjoy/previous?via=https://www.jmenning.com">< Previous</a></td>
+		<td style="text-align: center"><a href="https://webri.ng/webring/cssjoy/random?via=https://www.jmenning.com">Random</a></td>
+		<td style="text-align: center"><a href="https://webri.ng/webring/cssjoy/next?via=https://www.jmenning.com">Next ></a></td>
+	</tr>
+</table>
+    </section>
+
     </div>
 </div>
 </div>
