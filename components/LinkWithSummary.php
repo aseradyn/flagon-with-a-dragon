114e4886ffe164626090c00d7e9119907e43a748
aseradyn <jill@jmenning.com>
2023-11-14 10:59:02 -0600

    Update LinkWithSummary.php

--- components/LinkWithSummary.php
+++ components/LinkWithSummary.php
@@ -1,6 +1,11 @@
 <?php
   function LinkWithSummary(string $title, string $summary, string $href) {
 ?>
+    <style>
+      .link-with-summary {
+        view-transition-name: article
+      }
+    </style>
     <div class="link-with-summary">
       <h3><a href="<?php echo $href ?>"></a><?php echo $title ?></h3>
       <p><?php echo $summary ?></p>
