4607d323c5911519345e785ee24991def91931f3
Jill Menning <jill@jmenning.com>
2024-05-01 20:21:26 -0500

    Ephemera

---
+++ components/webring.php
@@ -0,0 +1,10 @@
+<table>
+	<tr>
+		<td colspan="3">This site is a member of the CS.Sjoy.lol webring</td>
+	</tr>
+	<tr>
+		<td style="text-align: center"><a href="https://webri.ng/webring/cssjoy/previous?via=https://www.jmenning.com">< Previous</a></td>
+		<td style="text-align: center"><a href="https://webri.ng/webring/cssjoy/random?via=https://www.jmenning.com">Random</a></td>
+		<td style="text-align: center"><a href="https://webri.ng/webring/cssjoy/next?via=https://www.jmenning.com">Next ></a></td>
+	</tr>
+</table>
